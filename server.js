6c79db2430422ea64509c08fecbb990172be7ea6
Simon <simonrosenberg1@gmail.com>
2021-07-06 12:49:29 +0100

    Compiled typescript

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ var server = new ApolloServer({
     resolvers: resolvers,
     context: function () {
         return { db: db };
-    }
+    },
+    introspection: true,
+    playground: true
 });
 sequelize.sync().then(function () {
     server.listen(process.env.PORT).then(function () {
