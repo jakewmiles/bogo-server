4806287dd8dd38020e9d45024f45a89c383474b9
Simon <simonrosenberg1@gmail.com>
2021-07-06 11:36:57 +0100

    Rerun typescript compiler

--- models/index.ts
+++ models/index.ts
@@ -9,9 +9,9 @@ const { UserAlbumFactory, UserAlbumStatic } = require('./user-album')
 const { MessagesFactory, MessagesStatic } = require('./messages')
 
 const sequelize = new Sequelize(
-  (process.env.DB_NAME = "bogo"),
-  (process.env.DB_USER = "postgres"),
-  (process.env.DB_PASSWORD = "postgres"),
+  (process.env.DB_NAME),
+  (process.env.DB_USER),
+  (process.env.DB_PASSWORD),
   {
     port: Number(process.env.DB_PORT) || 5432,
     host: process.env.DB_HOST || "localhost",
